610e6f2433b19d4046678d0d649c37bc858f93ce
ananyaasris <138882907+ananyaasris@users.noreply.github.com>
2026-07-16 22:04:38 +0530

    Update GatewayApplication.java

--- gateway-service/src/main/java/com/github/yildizmy/GatewayApplication.java
+++ gateway-service/src/main/java/com/github/yildizmy/GatewayApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -14,7 +15,6 @@ import java.net.UnknownHostException;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-@RequestMapping("/gateway-service")
 public class GatewayApplication {
 
     public static void main(String[] args) {
